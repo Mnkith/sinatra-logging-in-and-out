61723ac472bf85c94a5e2272e46c73ed10f771b5
Mnkith <munkithabid@gmail.com>
2021-01-22 15:01:10 -0600

    commit tree d45a48b3319a410e61260fdda1734625308a6ede

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,34 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    # User.create(username: "MUNKITH", password: "Abid")
     erb :index
   end
 
   post '/login' do
-
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in? session
+      @sess = session
+    # binding.pry
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
 
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
+# require_relative '../models/user.rb'
 class Helpers
+  def self.current_user(sess_h)
+    User.find(sess_h[:user_id])
+  end
+
+  def self.is_logged_in? sess_h
+    !!sess_h[:user_id]
+  end
 end
